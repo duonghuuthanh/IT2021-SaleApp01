18b6466bda6e958e4dad17f4883df7b2e2d8fbca
dhthanhqa@gmail.com <dhthanhqa@gmail.com>
2023-12-20 16:45:15 +0700

    update cart

--- saleappv2/app/models.py
+++ saleappv2/app/models.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, Enum
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Enum, Boolean, DateTime
 from sqlalchemy.orm import relationship
 from app import db, app
 from flask_login import UserMixin
 import enum
+from datetime import datetime
 
 
 class UserRoleEnum(enum.Enum):
@@ -18,6 +19,7 @@ class User(db.Model, UserMixin):
     avatar = Column(String(100),
                     default='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
     user_role = Column(Enum(UserRoleEnum), default=UserRoleEnum.USER)
+    receipts = relationship('Receipt', backref='user', lazy=True)
 
     def __str__(self):
         return self.name
@@ -40,39 +42,60 @@ class Product(db.Model):
     price = Column(Float, default=0)
     image = Column(String(100))
     category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
+    receipt_details = relationship('ReceiptDetails', backref='product', lazy=True)
 
     def __str__(self):
         return self.name
+
+
+class BaseModel(db.Model):
+    __abstract__ = True
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    active = Column(Boolean, default=True)
+    created_date = Column(DateTime, default=datetime.now())
+
+
+class Receipt(BaseModel):
+    user_id = Column(Integer, ForeignKey(User.id), nullable=False)
+    receipt_details = relationship('ReceiptDetails', backref='receipt', lazy=True)
+
+
+class ReceiptDetails(BaseModel):
+    quantity = Column(Integer, default=0)
+    price = Column(Float, default=0)
+    receipt_id = Column(Integer, ForeignKey(Receipt.id), nullable=False)
+    product_id = Column(Integer, ForeignKey(Product.id), nullable=False)
 
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-        import hashlib
-        u = User(name='Admin', username='admin',
-                 password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()),
-                 user_role=UserRoleEnum.ADMIN)
-        db.session.add(u)
-        db.session.commit()
-
-        c1 = Category(name='Mobile')
-        c2 = Category(name='Tablet')
-
-        db.session.add(c1)
-        db.session.add(c2)
-        db.session.commit()
-
-        p1 = Product(name='iPhone 13', price=20000000, category_id=1,
-                     image="https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg")
-        p2 = Product(name='Galaxy S23', price=21000000, category_id=1,
-                     image="https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg")
-        p3 = Product(name='iPad Pro 2023', price=22000000, category_id=2,
-                     image="https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg")
-        p4 = Product(name='Galaxy Tab S9', price=28000000, category_id=2,
-                     image="https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg")
-        p5 = Product(name='Note 13', price=20000000, category_id=1,
-                     image="https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg")
-
-        db.session.add_all([p1, p2, p3, p4, p5])
-        db.session.commit()
+        # import hashlib
+        # u = User(name='Admin', username='admin',
+        #          password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()),
+        #          user_role=UserRoleEnum.ADMIN)
+        # db.session.add(u)
+        # db.session.commit()
+        #
+        # c1 = Category(name='Mobile')
+        # c2 = Category(name='Tablet')
+        #
+        # db.session.add(c1)
+        # db.session.add(c2)
+        # db.session.commit()
+        #
+        # p1 = Product(name='iPhone 13', price=20000000, category_id=1,
+        #              image="https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg")
+        # p2 = Product(name='Galaxy S23', price=21000000, category_id=1,
+        #              image="https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg")
+        # p3 = Product(name='iPad Pro 2023', price=22000000, category_id=2,
+        #              image="https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg")
+        # p4 = Product(name='Galaxy Tab S9', price=28000000, category_id=2,
+        #              image="https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg")
+        # p5 = Product(name='Note 13', price=20000000, category_id=1,
+        #              image="https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg")
+        #
+        # db.session.add_all([p1, p2, p3, p4, p5])
+        # db.session.commit()
